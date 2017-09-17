378686b88b121f7e133190fbfd38a21d04e287df
mxsxs2 <mxsxs2@gmail.com>
2017-09-17 19:57:35 +0100

    Current Time

---
+++ 2Current_time/current_time.go
@@ -0,0 +1,13 @@
+//Write a program that prints the current time and date to the console.
+//It is the main package
+package main
+//import format and time
+import(
+	"fmt"
+	"time"
+)
+//Main function
+func main() {
+	//Print the time
+	fmt.Println("The time is", time.Now())
+}
