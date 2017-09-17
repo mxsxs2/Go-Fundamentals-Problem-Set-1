c19192dbf4fc5d80e85065744a7df9aed748aa4c
mxsxs2 <mxsxs2@gmail.com>
2017-09-17 19:53:19 +0100

    Japanese Hello World

---
+++ 1Konnichiwa/helloworld.go
@@ -0,0 +1,10 @@
+//Write a program that prints “Hello, world!” in Japanese (using Japanese characters) to the screen.
+//It is the main package
+package main
+//import format
+import "fmt"
+//Main function
+func main() {
+	//Print a line
+	fmt.Println("こんにちは世界") //If we can trust google translate
+}
