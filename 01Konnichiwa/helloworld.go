e62d0bb1ffe9c6622250de01658858177be878f3
KRISZTIAN NAGY - Student <g00330024@gmit.ie>
2017-10-03 15:48:47 +0100

    Re design factorial program

--- 01Konnichiwa/helloworld.go
+++ 01Konnichiwa/helloworld.go
@@ -1,10 +1,16 @@
 //Write a program that prints “Hello, world!” in Japanese (using Japanese characters) to the screen.
 //It is the main package
 package main
+
 //import format
-import "fmt"
+import (
+	"fmt"
+)
+
 //Main function
 func main() {
+	//Hello world in Japanase, if we can trust google translate
+	hello := "こんにちは世界"
 	//Print a line
-	fmt.Println("こんにちは世界") //If we can trust google translate
+	fmt.Println(hello)
 }
--- 04Factorial_digit_sum/factorial_digit_sum.go
+++ 04Factorial_digit_sum/factorial_digit_sum.go
@@ -16,38 +16,13 @@ import (
 
 //Main function
 func main() {
+	//Get the number from the arguments
+	num := getNumber()
+	//claclutae factorial and add the numbers together
+	sum := getSumofBigInt(getFactorialofnum(num))
 
-	num := int64(100)
-	//Check if there is any argument supplied
-	if len(os.Args) > 1 {
-		//Parse the incoming command
-		n, _ := strconv.ParseInt(os.Args[1], 10, 0)
-		num = n
-	}
 	//Check if the given number is higher than 0
 	if num > 0 {
-
-		//Factorial holder
-		factorial := big.NewInt(num)
-		//Sum of it
-		var sum int = 0
-		//Loop the 100
-		for i := 100; i > 0; i-- {
-			//Multiply n-1
-			factorial.Mul(factorial, big.NewInt(int64(i)))
-		}
-
-		//Holder for the number at position
-		n := new(big.Int)
-
-		//Loop until the end of the number
-		for factorial.BitLen() > 0 {
-			//Calculation: factorial/(position on power of 10)%10
-			//And save to n
-			factorial.DivMod(factorial, new(big.Int).SetUint64(uint64(10)), n)
-			//Add n to the sum
-			sum += int(n.Uint64())
-		}
 		//Print the sum
 		fmt.Println("The sum of factorial", num, " is :", sum)
 	} else {
@@ -56,4 +31,43 @@ func main() {
 	fmt.Println("\nYou can specify which number to use for the factorial by adding a number after the name of the runnable in terminal.")
 	fmt.Println("For example: ./factorial_digit_sum.exe 101")
 
+}
+
+//Function used to get the command line argument or return 100
+func getNumber() int64 {
+	num := int64(100)
+	//Check if there is any argument supplied
+	if len(os.Args) > 1 {
+		//Parse the incoming command
+		n, _ := strconv.ParseInt(os.Args[1], 10, 0)
+		num = n
+	}
+	return num
+}
+
+//Function used to get a factorial of a number
+func getFactorialofnum(num int64) *big.Int {
+	//Factorial holder
+	factorial := new(big.Int)
+	//Do factorial
+	return factorial.MulRange(1, num)
+}
+
+//Function used to return the sum of a big int
+func getSumofBigInt(factorialNumber *big.Int) int {
+	//Sum of it
+	sum := 0
+	//Holder for the number at position
+	n := new(big.Int)
+
+	//Do factorial and loop it's numbers
+	for factorialNumber.BitLen() > 0 {
+		//Calculation: factorial/(position on power of 10)%10
+		//And save to n
+		factorialNumber.DivMod(factorialNumber, new(big.Int).SetUint64(uint64(10)), n)
+		//Add n to the sum
+		sum += int(n.Uint64())
+	}
+
+	return sum
 }
