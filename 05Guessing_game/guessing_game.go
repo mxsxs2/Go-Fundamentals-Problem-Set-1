495e25974373c9fab03b2f444bf02b17a65c8e72
mxsxs2 <mxsxs2@gmail.com>
2017-10-06 21:02:25 +0100

    Optimalize functions. Allow user to set the range for the random number generator

--- 05Guessing_game/guessing_game.go
+++ 05Guessing_game/guessing_game.go
@@ -1,50 +1,138 @@
-/*Write a guessing game where the user must guess a secret number. 
-  After every guess the program tells the user whether their number was too large or too small. 
-  At the end the number of tries needed should be printed. It counts only as one try if they input the same number multiple times consecutively.*/
+/*Write a guessing game where the user must guess a secret number.
+  After every guess the program tells the user whether their number was too large or too small.
+  At the end the number of tries needed should be printed. It counts only as one try if they input the same number multiple times consecutively.
+
+ Used this link as an example for returning errors and error handling in general
+https://gobyexample.com/errors
+How to use command line arguments
+https://gobyexample.com/command-line-arguments
+
+*/
 
 //It is the main package
 package main
+
 //imports
-import(
+import (
+	"errors"
 	"fmt"
+	"math"
 	"math/rand"
+	"os"
+	"strconv"
 	"time"
 )
+
 //Main function
 func main() {
-	//Seed random so it has less chance to give the same numbers
-	rand.Seed(time.Now().Unix())
-	//The number, max 100
-	number:=rand.Intn(100)
 	//Num of tries
-	var tries int = 0
+	tries := 0
 	//Last try
-	var lastTry int = -1
-	//Previous try
-	var prevTry int = -1
+	var lastTry int
+	//Holder for the tried numbers
+	var triedNumbers []int
 
-	fmt.Println("Please guess the number which is between 0 and 100")
+	//Get the random number
+	number, max, err := getNumber()
+	//Let the user know if there was any parsing error
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Please guess the number which is between 0 and", max)
 
 	//Loop until the number is found
-	for number!=lastTry{
-		//Set the previous try
-		prevTry=lastTry
-		//Read in the number
-		fmt.Scanf("%d",&lastTry)
-		//Check if the number is smaller than try
-		if(lastTry>number){
-			fmt.Println(lastTry,"is larger")
-		//Check if number is bigger than try
-		}else if(lastTry<number){
-			fmt.Println(lastTry,"is smaller")
+	for number != lastTry {
+		//Read in the number from the user into the lastTry variable
+		readInt(&lastTry)
+
+		//Compare the numbers
+		cmp := compareNumbers(lastTry, number)
+
+		//Check if this number was already tried
+		if findNumberInSlice(lastTry, triedNumbers) {
+			fmt.Println("You already tried", lastTry)
+		} else {
+			//Increase the number of tries if the tried number is not the same as previous
+			tries++
+			//Add the number to the tried numbers slice
+			triedNumbers = append(triedNumbers, lastTry)
 		}
 
-		//Increase the number of tries if the tried number is not the same as previous
-		//In my understanding i didn't need to store them in an array and count every number only once
-		if(prevTry!=lastTry){
-			tries++
+		//Check if the number is smaller than try
+		if cmp == -1 {
+			fmt.Println("The guessed number", lastTry, "is larger")
+			//Check if number is bigger than try
+		} else if cmp == 1 {
+			fmt.Println("The guessed number", lastTry, "is smaller")
 		}
+
 	}
 
-	fmt.Println("Congratualtions you guessed the number with",tries,"tries")
+	fmt.Println("Congratualtions you guessed the number with", tries, "tries")
+}
+
+//Function used to get the command line argument and return a random number between 0 and the command line argument.
+//If the argument is invlaid or less than 0 then it createsthe random between 0 and 100
+func getNumber() (int, int, error) {
+	//Seed random so it has less chance to give the same numbers
+	rand.Seed(time.Now().Unix())
+
+	//Check if there is any argument supplied
+	if len(os.Args) > 1 {
+		//Parse the incoming command and check if it can be converted to an int
+		if n, err := strconv.ParseInt(os.Args[1], 10, 0); err == nil && n > 0 {
+			//Return a random number between 0 and n. Return no error
+			//The numbers converted to int from int64
+			return rand.Intn(int(n)), int(n), nil
+		} else {
+			//Return a random number between 0 and 100. Set number and the error
+			return rand.Intn(100), 100, errors.New("The command line input number could not be parsed or it is 0.\n Default 100 is used")
+		}
+
+	}
+	//Return a random number between 0 and 100. No error if there was no command line input
+	return rand.Intn(100), 100, nil
+}
+
+//Function used to read in a integer with input validation
+func readInt(pointer *int) {
+	//Read in the number
+	if _, err := fmt.Scanf("%d", pointer); err != nil {
+		//If there was an error then rread again
+		fmt.Println("Invalid input. Please enter a number again:")
+		//Re read the number
+		readInt(pointer)
+	}
+}
+
+//Function used to find a number in a slice
+func findNumberInSlice(number int, slice []int) bool {
+	//Get the lenght of the slice for the loop
+	sliceLenght := len(slice)
+	//Get the middle of the slice. It is rounded up so it will work with even 1 element
+	middleOfSlice := int(math.Ceil(float64(sliceLenght) / 2))
+	//Do a loop which goes from the back and from the front as well
+	//"Devide and conquer" as John Healy said always in Data Structures and Agorithms class
+	for i := 0; middleOfSlice > i; i++ {
+		//Check if the the number is the same as any of the two current ones
+		if slice[i] == number || slice[sliceLenght-1-i] == number {
+			return true
+		}
+	}
+	return false
+}
+
+//Function to check if the guessed number is larger or smaller
+//If the first number is higher it returns -1
+//If the numbers are the same then return 0
+//If the second number is higher then returns 1
+//It works the same way as the string compare in C
+func compareNumbers(num1 int, num2 int) int {
+	if num1 > num2 {
+		return -1
+	} else if num1 == num2 {
+		return 0
+	} else {
+		return 1
+	}
 }
