e30efe5ab1c863868fc559e5bb6fb4cd7c75a3fb
mxsxs2 <mxsxs2@gmail.com>
2017-10-06 23:41:11 +0100

    Reformat the time and date

--- 02Current_time/current_time.go
+++ 02Current_time/current_time.go
@@ -1,13 +1,19 @@
 //Write a program that prints the current time and date to the console.
 //It is the main package
 package main
+
 //import format and time
-import(
+import (
 	"fmt"
 	"time"
 )
+
 //Main function
 func main() {
+	//Get the time
+	ctime := time.Now()
 	//Print the time
-	fmt.Println("The time is", time.Now())
+	fmt.Println("The time is", ctime.Format("15:04"))
+	//Print the date
+	fmt.Println("The current date is", ctime.Format(" 02/01/2006"))
 }
