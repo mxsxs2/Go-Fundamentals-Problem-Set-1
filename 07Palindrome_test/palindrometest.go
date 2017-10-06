157fc259ca75464e726d640a22c5ade7ba4960da
mxsxs2 <mxsxs2@gmail.com>
2017-10-06 21:50:29 +0100

    Change to buffered reader. Request string again if nothing is entered

--- 07Palindrome_test/palindrometest.go
+++ 07Palindrome_test/palindrometest.go
@@ -2,36 +2,38 @@
 
 //It is the main package
 package main
+
 //imports
-import(
+import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
+
 //Main function
 func main() {
-	//The input string
-	var input string
 
 	fmt.Println("Please input a word to check if it is a palindrome:")
 	//Get the input
-	fmt.Scanf("%s\n", &input)
+	input := getInputString()
 	//Check if it is palindrome
-	if(isPalindrome(strings.ToLower(input))){
+	if isPalindrome(strings.ToLower(input)) {
 		fmt.Println("Your text is palindrome")
-	}else{
+	} else {
 		fmt.Println("Your text is not palindrome")
 	}
 }
 
-
 //Function used to check if a text is palindrome
-func isPalindrome(text string) bool{
-	var front int = 0
-	back:= len(text)-1
+func isPalindrome(text string) bool {
+	//Set the two directions of the loop
+	front := 0
+	back := len(text) - 1
 	//Loop until the two numbers meet
-	for front<back{
-		//If there is any difference then it is not a palindrome, also works with UTF-8
-		if(string([]rune(text)[front])!=string([]rune(text)[back])){
+	for front < back {
+		//If there is any difference then it is not a palindrome. It is converted into runes for more simple number comparison
+		if rune(text[front]) != rune(text[back]) {
 			return false
 		}
 		front++
@@ -39,4 +41,21 @@ func isPalindrome(text string) bool{
 	}
 	//It is a palindrome
 	return true
+}
+
+//Function used to get the input string
+func getInputString() string {
+	//Create the reader
+	reader := bufio.NewReader(os.Stdin)
+	//Get the input string
+	input, err := reader.ReadString('\n')
+	//Trim the input so no trailing space or \n is added
+	input = strings.TrimSpace(input)
+	//Check if there is anything entered
+	if err != nil || len(input) < 1 {
+		fmt.Println("You should write something! Please try again:")
+		//Get the string again
+		getInputString()
+	}
+	return input
 }
